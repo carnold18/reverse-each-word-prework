dd83448602ce990da95566081b5c185f060f80ba
Christopher Arnold <chrisarnold2010@gmail.com>
2018-07-08 23:16:05 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+def reverse_each_word(string)
+  array = string.split
+  array.each do |i|
+    puts i.reverse!
+  end
+  return array.join(" ")
+end
+
+def reverse_each_word(string)
+  array = string.split
+  array.collect { |x| x.reverse!}
+  return array.join(" ")
+end
